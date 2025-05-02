efad4ac9218fc9caeae5c2b5f3d8cc4880304bd9
Arturo Villalta <alvillalt@gmail.com>
2025-05-02 08:24:48 +0000

    Termino el proyecto Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,32 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { EvaluatedModules } from "vite/module-runner";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+window.onload = function () {
+
+  // Declaración variables
+  let pronoun = ["mis", "las",];
+  let adj = ["mejores", "peores"];
+  let noun = ["vacaciones", "sorpresas"];
+  let extension = [".com", ".es"]
+
+  // Iteración + Concatenación
+  let domainSum = "";
+  pronoun.map((element) => {
+    adj.map((iterator) => {
+      noun.map((item) => {
+        extension.map((extension) => {
+          domainSum += `<li>${element}${iterator}${item}${extension}</li>`;
+        })
+      })
+    })
+  })
+
+  // Captura elemento HTML
+  const domainGenerator = document.querySelector("#domainName");
+  domainGenerator.innerHTML = domainSum;
+  console.log(domainSum);
+
+}
