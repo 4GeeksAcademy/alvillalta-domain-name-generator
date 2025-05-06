dae921e83c26d71f6da6cb4ebab2fa62a7922e5a
Arturo Villalta <alvillalt@gmail.com>
2025-05-06 18:39:12 +0000

    Actualizo proyecto con modificaciones en el nombre de las variables, índices del map y sin comentarios

--- src/app.js
+++ src/app.js
@@ -9,27 +9,23 @@ import { EvaluatedModules } from "vite/module-runner";
 
 window.onload = function () {
 
-  // Declaración variables
-  let pronoun = ["mis", "las",];
-  let adj = ["mejores", "peores"];
-  let noun = ["vacaciones", "sorpresas"];
+  let pronombre = ["mis", "las",];
+  let adjetivo = ["mejores", "peores"];
+  let sustantivo = ["vacaciones", "sorpresas"];
   let extension = [".com", ".es"]
 
-  // Iteración + Concatenación
   let domainSum = "";
-  pronoun.map((element) => {
-    adj.map((iterator) => {
-      noun.map((item) => {
-        extension.map((extension) => {
-          domainSum += `<li>${element}${iterator}${item}${extension}</li>`;
+  pronombre.map((elementoPronombre) => {
+    adjetivo.map((elementoAdjetivo) => {
+      sustantivo.map((elementoSustantivo) => {
+        extension.map((elementoExtension) => {
+          domainSum += `<li>${elementoPronombre}${elementoAdjetivo}${elementoSustantivo}${elementoExtension}</li>`;
         })
       })
     })
   })
 
-  // Captura elemento HTML
   const domainGenerator = document.querySelector("#domainName");
   domainGenerator.innerHTML = domainSum;
   console.log(domainSum);
-
 }
